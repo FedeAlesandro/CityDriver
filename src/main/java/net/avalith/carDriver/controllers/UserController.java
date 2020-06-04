e16076f33346c273187c044d5148050dc9edb061
FedeAlesandro <fede.alesandro@gmail.com>
2020-06-04 12:45:10 -0300

    [Fix] Mapping added

--- src/main/java/net/avalith/carDriver/controllers/UserController.java
+++ src/main/java/net/avalith/carDriver/controllers/UserController.java
@@ -4,6 +4,8 @@ import net.avalith.carDriver.models.User;
 import net.avalith.carDriver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +18,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PostMapping("/")
     public ResponseEntity<User> save(User user){
         return ResponseEntity.ok(userService.save(user));
     }
 
+    @GetMapping("/")
     public ResponseEntity<List<User>> getAll(){
         List<User> users = userService.getAll();
         if(users.isEmpty())
