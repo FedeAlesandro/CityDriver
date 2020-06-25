878c69577af0abab4f7e90967e256fa6e488fdc4
MauroYini <mauro8792yini@gmail.com>
2020-06-25 12:33:01 -0300

    faltaba una llave

--- src/test/java/net/avalith/carDriver/factoryService/FactoryService.java
+++ src/test/java/net/avalith/carDriver/factoryService/FactoryService.java
@@ -7,11 +7,9 @@ import net.avalith.carDriver.models.Point;
 import net.avalith.carDriver.models.Provider;
 import net.avalith.carDriver.models.dtos.CityDto;
 import net.avalith.carDriver.models.dtos.requests.BrandDtoRequest;
+import net.avalith.carDriver.models.dtos.requests.PointDtoRequest;
+import net.avalith.carDriver.models.dtos.requests.PointDtoUpdateRequest;
 import net.avalith.carDriver.models.dtos.requests.ProviderDtoRequest;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 
 public interface FactoryService {
 
@@ -46,19 +44,21 @@ public interface FactoryService {
         return new BrandDtoRequest("name",Boolean.TRUE);
     }
 
-    default Provider createProvider(){
+    default Provider createProvider() {
         return new Provider("toyoya");
+    }
 
     default PointDtoRequest createPointDtoRequest(){
         return new PointDtoRequest(false, "30", "20", 20, 15, "Mar del Plata");
     }
 
     default PointDtoUpdateRequest createPointDtoUpdateRequest(){
+
         return new PointDtoUpdateRequest(false, false, "30", "20", 20, 15, "Mar del Plata");
     }
 
     default Point createPoint(){
-        return new Point(createPointDtoRequest(), createCity());
 
+        return new Point(createPointDtoRequest(), createCity());
     }
 }
