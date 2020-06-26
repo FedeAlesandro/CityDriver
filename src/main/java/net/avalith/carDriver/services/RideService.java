8399854679dd45584104e0f4d7d73dd82803c79b
FedeAlesandro <fede.alesandro@gmail.com>
2020-06-26 13:06:41 -0300

    [Feature] Task 67

--- src/main/java/net/avalith/carDriver/services/RideService.java
+++ src/main/java/net/avalith/carDriver/services/RideService.java
@@ -3,12 +3,14 @@ package net.avalith.carDriver.services;
 import net.avalith.carDriver.exceptions.NotFoundException;
 import net.avalith.carDriver.models.Point;
 import net.avalith.carDriver.models.Ride;
+import net.avalith.carDriver.models.RideLog;
 import net.avalith.carDriver.models.User;
 import net.avalith.carDriver.models.Vehicle;
 import net.avalith.carDriver.models.dtos.RidePointDto;
 import net.avalith.carDriver.models.dtos.requests.RideDtoRequest;
 import net.avalith.carDriver.models.dtos.requests.RideDtoUpdateRequest;
 import net.avalith.carDriver.repositories.PointRepository;
+import net.avalith.carDriver.repositories.RideLogRepository;
 import net.avalith.carDriver.repositories.RideRepository;
 import net.avalith.carDriver.repositories.UserRepository;
 import net.avalith.carDriver.repositories.VehicleRepository;
@@ -37,6 +39,9 @@ public class RideService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RideLogRepository rideLogRepository;
+
     public Ride save(RideDtoRequest ride){
 
         RidePointDto ridePoint = ride.getOriginPoint();
@@ -50,7 +55,11 @@ public class RideService {
         User user = userRepository.getByDni(ride.getUserDni())
                 .orElseThrow(() -> new NotFoundException(NOT_FOUND_USER));
 
-        return rideRepository.save(new Ride(ride, vehicle, point, user));
+        Ride rideSaved = rideRepository.save(new Ride(ride, vehicle, point, user));
+
+        rideLogRepository.save(new RideLog(rideSaved.getId(), rideSaved.getState()));
+
+        return rideSaved;
     }
 
     public List<Ride> getAll(){
@@ -81,6 +90,8 @@ public class RideService {
         Ride rideUpdate = new Ride(ride, vehicle, originPoint, destinationPoint, user);
         rideUpdate.setId(id);
         rideUpdate.setCode(oldRide.getCode());
+
+        rideLogRepository.save(new RideLog(id, rideUpdate.getState()));
 
         return rideRepository.save(rideUpdate);
     }
