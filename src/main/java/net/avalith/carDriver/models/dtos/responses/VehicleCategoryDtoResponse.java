d47aeff1a9174acbdf41679409191fceeb1dd74b
MauroYini <mauro8792yini@gmail.com>
2020-07-02 11:45:34 -0300

    lombok, no me toma los get

--- src/main/java/net/avalith/carDriver/models/dtos/responses/VehicleCategoryDtoResponse.java
+++ src/main/java/net/avalith/carDriver/models/dtos/responses/VehicleCategoryDtoResponse.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.avalith.carDriver.models.VehicleCategory;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 public class VehicleCategoryDtoResponse {
@@ -12,9 +14,18 @@ public class VehicleCategoryDtoResponse {
 
     private Double commission;
 
+    private Double priceHour;
+
+    private Double priceDay;
+
+    private Double priceWeek;
+
 
     public VehicleCategoryDtoResponse(VehicleCategory vehicleCategory) {
         this.name = vehicleCategory.getName();
         this.commission = vehicleCategory.getCommission();
+        this.priceHour = vehicleCategory.getPriceHour();
+        this.priceDay = vehicleCategory.getPriceDay();
+        this.priceWeek = vehicleCategory.getPriceWeek();
     }
 }
