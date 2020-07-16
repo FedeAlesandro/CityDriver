38979e3389234c679ab857e51caa26355fab0305
MauroYini <mauro8792yini@gmail.com>
2020-07-16 11:32:58 -0300

    mishaps

--- src/main/java/net/avalith/carDriver/controllers/MishapController.java
+++ src/main/java/net/avalith/carDriver/controllers/MishapController.java
@@ -2,19 +2,18 @@ package net.avalith.carDriver.controllers;
 
 
 import net.avalith.carDriver.models.Mishap;
+import net.avalith.carDriver.models.dtos.responses.MishapDtoResponse;
 import net.avalith.carDriver.services.MishapService;
 import net.avalith.carDriver.utils.Routes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = Routes.MISHAP)
@@ -24,12 +23,17 @@ public class MishapController {
     private MishapService mishapService;
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<List<Mishap>> getAll(){
+    public ResponseEntity<List<MishapDtoResponse>> getAll(){
         List<Mishap> listMishaps = mishapService.getAll();
         if (listMishaps.isEmpty()){
             return ResponseEntity.noContent().build();
-        }else
-            return ResponseEntity.ok(listMishaps);
+        }else{
+            List<MishapDtoResponse> listMishapResponse = listMishaps.stream()
+                    .map(MishapDtoResponse::new)
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.ok(listMishapResponse);
+        }
     }
 /*    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Mishap> save(@RequestBody Mishap mishap, @PathVariable("idRide") Long idRide){
